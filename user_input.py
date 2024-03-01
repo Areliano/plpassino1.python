d9d300f842aa9f96089f90e7b131bfad3805e81f
Fatma <fatmahussein355@gmail.com>
2024-03-02 01:42:33 +0300

    python_plp

---
+++ user_input.py
@@ -0,0 +1,4 @@
+NAME = input('ENTER YOUR NAME: ')
+AGE = input('ENTER YOUR AGE: ')
+LOCATION = input('ENTER YOUR LOCATION: ')
+print(f'HELLO! Your name is{NAME}, you are {AGE} years old and you live in {LOCATION}.')
